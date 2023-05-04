731395fd9e4c429a9ad24a6c4317d3bf829f4da8
MDinocera <micheledinocera@gmail.com>
2023-05-04 16:46:41 +0200

    linted

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/cryptid-companion/' : '/',
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/cryptid-companion/" : "/",
   pluginOptions: {
     i18n: {
       locale: "it",
